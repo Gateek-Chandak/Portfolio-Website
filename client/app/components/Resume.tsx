d7923a698aece88ed0050ce9d936e6a533ecab86
Gateek <chandakgateek@gmail.com>
2024-09-07 16:50:01 -0400

    made website responsive

--- client/app/components/Resume.tsx
+++ client/app/components/Resume.tsx
@@ -1,11 +1,11 @@
 const ResumeSection = () => {
     return ( 
-        <div className="h-screen w-full flex justify-center items-center pt-14 snap-center scroll-container" id="resume">
+        <div className="h-screen relative w-9/12 m-auto snap-start scroll-container" id="resume">
             <iframe
-                src="/Gateek_Chandak_Resume_External.pdf"
-                className=""
-                style={{ border: 'none', width: '60%', height: "650px" }}
-            />
+                src='/Gateek_Chandak_Resume_External.pdf'
+                title="PDF Viewer"
+                className="absolute top-24 left-0 w-full h-5/6 border-none"
+            ></iframe>
         </div>
      );
 }
